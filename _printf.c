ea6650a6513c9bab485270248b999d88446a3862
Adewale Fatoki <adewaleftk@gmail.com>
2022-11-21 14:14:27 +0100

    _printf

--- _printf.c
+++ _printf.c
@@ -1,41 +1,70 @@
 #include "main.h"
 
 /**
- * _printf - prints a string in a formatted way
- * @format: string to print
- * @...: variadic parameters
+ * print_string_slice - print a slice of a string
  *
+ * @str: string
+ * @a: start index
+ * @b: end index
  * Return: number of characters printed
  */
+int print_string_slice(char *str, int a, int b)
+{
+	int len = b - a;
 
+	while (a < b)
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	return (len);
+}
+
+/**
+ * _printf - print a formatted string
+ *
+ * @format: the format of the string
+ *
+ * Return: the number of characters printed
+ */
 int _printf(const char *format, ...)
 {
-	int i = 0;
-	int count = 0;
-	int value = 0;
+	int len = 0, i = 0, start_index;
+	va_list ap;
+	char *p = (char *)format;
 
-	/*Prevent parsing a null pointer*/
-	if (format == NULL)
+	va_start(ap, format);
+
+	if (format == NULL || (format[i] == '%' && format[i + 1] == '\0'))
 		return (-1);
 
-	/*Print each character of string*/
 	while (format[i])
 	{
-		if (format[i] != '%')
-		{
-			value = write(1,&format[i],1);
-			count = count + value;
-			i++;
-			continue;
-		}
-
 		if (format[i] == '%')
 		{
-			printf("I have encountered a percent\n");
-			//kjg
-			break;
-		}
-	}
+			int (*specifier_func)(va_list, params_t *);
+			params_t *params = init_params();
 
-	return (count);
+			start_index = i;
+			i++;
+			i += get_flags((p + i), params);
+			i += get_width((p + i), ap, params);
+
+			specifier_func = get_specifier_func(*(p + i));
+
+			if (specifier_func != NULL)
+			{
+				len += specifier_func(ap, params);
+				i++;
+			}
+			else
+				len += print_string_slice(p, start_index, i);
+			free_params(params);
+			continue;
+		}
+		len += _putchar(format[i]);
+		i++;
+	}
+	va_end(ap);
+	return (len);
 }
---
+++ check_specifiers.c
@@ -0,0 +1,12 @@
+/**
+ * check_specifier - check that characters is a valid specifier and
+ * assigns an appropriate function for its printing.
+ * @format: the specifier (char*)
+ *
+ * Return: pointer to a function
+ */
+
+int (*check_specifier(char*))(va_list)
+{
+	//expressions
+}
